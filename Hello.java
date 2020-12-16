d3cf035f760d8f27d685a725fa207e74210b7ad1
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 00:14:47 +0100

    More about variables - more examples

--- Hello.java
+++ Hello.java
@@ -1,19 +1,24 @@
 public class Hello {
     public static void main(String[] args) {
         
+        // Primitive types
         byte myByte = 12; // [-128, 127] B:1
         short myShort = 10234; // [-32k, 32k] B:2
-        int myAge = 28; // [-2b, 2b] B:4
-        long veryBigNumbers = 38903892; // [??] B: 8
+        int myAge = 28_321_321; // [-2b, 2b] B:4
+        long veryBigNumbers = 38903892L; // [??] B: 8
 
         // number with decimal point
-        float variableName // B: 4
-        double anotherNameForVariable // B:8
+        float variableName; // B: 4
+        float price = 10.99F;
+
+        double anotherNameForVariable; // B:8
 
         char singleCharacter = 'a'; // B:2
-        boolean isTrue = 1; // Only true:1 and false:0
+        char letter = 'A';
+
+        boolean isThere = true; // Only true:1 and false:0
         
-        System.out.println(age);
+        System.out.println(myByte);
 
     }
 }
