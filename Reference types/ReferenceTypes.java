228d9c3f6baf67a3e402f0dcdfa79f9682091d52
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 00:30:29 +0100

    Reference type with actual date added

---
+++ Reference types/ReferenceTypes.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+import java.util.Date; // Package which import cause od Date.util class
+
+public class ReferenceTypes {
+    
+    public static void main (String[] args) {
+        byte age = 30; // Primitive type
+
+        Date now = new Date(); // now is instance of Date class
+        System.out.println(now);
+    }
+
+}
