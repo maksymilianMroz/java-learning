a20061e210c60dbae7ab0ab9c356e086ee6f6524
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 00:09:25 +0100

    Declaring variables

--- Hello.java
+++ Hello.java
@@ -1,5 +1,19 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        
+        byte myByte = 12; // [-128, 127] B:1
+        short myShort = 10234; // [-32k, 32k] B:2
+        int myAge = 28; // [-2b, 2b] B:4
+        long veryBigNumbers = 38903892; // [??] B: 8
+
+        // number with decimal point
+        float variableName // B: 4
+        double anotherNameForVariable // B:8
+
+        char singleCharacter = 'a'; // B:2
+        boolean isTrue = 1; // Only true:1 and false:0
+        
+        System.out.println(age);
+
     }
 }
