f24f77b19625ed79a88697858ec8dc2848ba5c83
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 23:32:34 +0100

    Creating an array

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,5 +1,13 @@
 public class Arrays {
     public static void main(String[] args) {
-        System.out.println("test");
+        int[] numbers = new int[5]; // Creating an array for 5 items
+
+        numbers[0] = 1;
+        numbers[1] = 2;
+        // ...
+        // numbers[10] = 3; // exepction
+
+        System.out.println(numbers); // Arrays will show as a chain of characters - place in memory where array was save
+
     }
 }
