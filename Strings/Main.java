825fcd8d8cfcbf1ddf61b474e2bdae2fdd2bcdaf
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 01:22:43 +0100

    Start learning strings stuff

---
+++ Strings/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = new String("Hello!"); // Can do this like this, but it's redundant
+
+        String betterMessege = "Hello world!";
+        System.out.println(betterMessege);
+    }
+}
---
+++ Strings/Strings.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String message = new String("Hello!"); // Can do this like this, but it's redundant
+
+        String betterMessege = "Hello world!";
+        System.out.println(betterMessege);
+    }
+}
