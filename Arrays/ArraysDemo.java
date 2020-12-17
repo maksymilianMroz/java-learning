f32cfa0f5bfbf15088d8c761083eaa97ab02acbe
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 23:45:46 +0100

    How to sort an array

--- Arrays/ArraysDemo.java
+++ Arrays/ArraysDemo.java
@@ -13,5 +13,14 @@ public class ArraysDemo {
         System.out.println(numbers); // will show as a chain of characters - place in memory where array was save
 
         System.out.println(Arrays.toString(numbers)); // Returns an array in [], and if I will not declare items of defined array, they will change to 0
+
+
+        int[] anotherArray = {1, 2, 3, 4, 5};
+        System.out.println(anotherArray.length);
+
+
+        int[] anotherArrayToSort = {12, 1, 7, 9999, 321};
+        Arrays.sort(anotherArrayToSort);
+        System.out.print(Arrays.toString(anotherArrayToSort)); // How to sort our array from lowest to greatest
     }
 }
