07f1f60e86ad54f5ee3dc2c0488e4b52080c3761
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-17 11:16:11 +0100

    Some string methods

--- Strings/Main.java
+++ Strings/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         String message = new String("Hello!"); // Can do this like this, but it's redundant
 
-        String betterMessege = "Hello world!";
+        String betterMessege = "Hello world!" + " Another text!";
+       
         System.out.println(betterMessege);
+        System.out.println(betterMessege.endsWith("$$$"));
+        System.out.println(betterMessege.length());
+        System.out.println(betterMessege.indexOf("A"));
+        System.out.println(betterMessege.replace("Ano", "%$#"));
     }
 }
