66d3113c6ee15d6f7f884a1a0272ddd7e29c0991
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-20 08:01:04 +0100

    How ++ operator works

--- ArithmeticExpressions/Arithmetic.java
+++ ArithmeticExpressions/Arithmetic.java
@@ -5,7 +5,12 @@ public class Arithmetic {
         // int result = (double)10 / (double)3; // It will give an error cause result is defined as int, but score is double:
 
         double resultAgain = (double)10 / (double)3; // That works!
-        System.out.println(resultAgain);
+        // System.out.println(resultAgain);
+
+        int x = 1;
+        // x++; // 2
+        ++x; // 2
+        System.out.println(x);
 
     }
 }
