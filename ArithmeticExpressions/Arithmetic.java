f8ef78331c2b93240aed33a38b11c2133385d417
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-22 14:55:24 +0100

    Incrementation

--- ArithmeticExpressions/Arithmetic.java
+++ ArithmeticExpressions/Arithmetic.java
@@ -4,13 +4,18 @@ public class Arithmetic {
 
         // int result = (double)10 / (double)3; // It will give an error cause result is defined as int, but score is double:
 
-        double resultAgain = (double)10 / (double)3; // That works!
+        // double resultAgain = (double)10 / (double)3; // That works!
         // System.out.println(resultAgain);
 
+        // int x = 1;
+        // // x++; // 2
+        // ++x; // 2
+
         int x = 1;
-        // x++; // 2
-        ++x; // 2
-        System.out.println(x);
+        int y = x++;
+        System.out.println(x); // in the line above I used x++ so even if I wanted to only create variable, I added 1 to x, too
+        System.out.println(y);
+        
 
     }
 }
