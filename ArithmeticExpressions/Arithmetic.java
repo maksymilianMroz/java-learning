a2bc0a216d849d5f23ed9eab0da4fbeb12821fb5
maksymilianMroz <mrozmaksymilian@gmail.com>
2020-12-22 15:19:28 +0100

    Augumented assigned operator

--- ArithmeticExpressions/Arithmetic.java
+++ ArithmeticExpressions/Arithmetic.java
@@ -16,10 +16,13 @@ public class Arithmetic {
         // System.out.println(x); // in the line above I used x++ so even if I wanted to only create variable, I added 1 to x, too
         // System.out.println(y);
 
-        int x = 1;
-        int y = ++x;
-        System.out.println(x);
-        System.out.println(y);
+        // int x = 1;
+        // int y = ++x;
+        // System.out.println(x);
+        // System.out.println(y);
 
+        int x = 1;
+        x += 2; // to samo co x = x + 2
+        System.out.println(x);
     }
 }
