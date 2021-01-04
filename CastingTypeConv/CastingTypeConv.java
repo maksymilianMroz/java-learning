788a86060c2f230ca0fa0a7118114f9214447c94
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-01-04 22:54:19 +0100

    How to cast string to number

--- CastingTypeConv/CastingTypeConv.java
+++ CastingTypeConv/CastingTypeConv.java
@@ -4,8 +4,16 @@ public class CastingTypeConv {
         // int y = x + 2;
         // System.out.println(y); // implicit casting byte > short > int > long
 
-        double x = 1.1;
-        int y = (int)x + 2;
+        // double x = 1.1;
+        // int y = (int)x + 2;
+        // System.out.println(y);
+
+        // If you want change string to number:
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
         System.out.println(y);
+        
+
+
     }
 }
