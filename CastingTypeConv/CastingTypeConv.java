0a9eea0fd06a2a78137851cfc0bae0d8181da069
maksymilianMroz <mrozmaksymilian@gmail.com>
2021-01-04 22:51:59 +0100

    Double casting to int

--- CastingTypeConv/CastingTypeConv.java
+++ CastingTypeConv/CastingTypeConv.java
@@ -1,7 +1,11 @@
 public class CastingTypeConv {
     public static void main(String[] args) {
-        short x = 1;
-        int y = x + 2;
-        System.out.println(y); // implicit casting byte > short > int > long
+        // short x = 1;
+        // int y = x + 2;
+        // System.out.println(y); // implicit casting byte > short > int > long
+
+        double x = 1.1;
+        int y = (int)x + 2;
+        System.out.println(y);
     }
 }
