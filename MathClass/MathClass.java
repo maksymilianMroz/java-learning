3803b21e783029bcd3cd1ed19da4a2ab44bb566e
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2021-01-09 13:33:47 +0100

    Floor method

--- MathClass/MathClass.java
+++ MathClass/MathClass.java
@@ -2,8 +2,14 @@ public class MathClass {
     public static void main(String[] args) {
         int roundEx = Math.round(1.1F);
         System.out.println(roundEx);
+        System.out.println("#########");
 
         int ceilEx = (int) Math.ceil(1.1F);
         System.out.println(ceilEx);
+        System.out.println("#########");
+
+        int floorEx = (int) Math.floor(1.1F);
+        System.out.println(floorEx);
+        System.out.println("#########");
     }
 }
