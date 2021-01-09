c925256bc478c07d3335d7a9ee2ef16ebd87d4b5
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2021-01-09 13:31:44 +0100

    Ceil method

--- MathClass/MathClass.java
+++ MathClass/MathClass.java
@@ -2,5 +2,8 @@ public class MathClass {
     public static void main(String[] args) {
         int roundEx = Math.round(1.1F);
         System.out.println(roundEx);
+
+        int ceilEx = (int) Math.ceil(1.1F);
+        System.out.println(ceilEx);
     }
 }
