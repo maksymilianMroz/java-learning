b9a8366b2d8c3340e697943e86733e5895127ce8
Maksymilian Mróz <mrozmaksymilian@gmail.com>
2021-01-25 23:07:30 +0100

    Few methods from number format

--- FormattingNumbers/FormattingNumbers.java
+++ FormattingNumbers/FormattingNumbers.java
@@ -1,5 +1,16 @@
+import java.text.NumberFormat;
+
 public class FormattingNumbers {
     public static void main(String[] args) {
-        System.out.println("Test");
+        // NumberFormat currency = NumberFormat.getCurrencyInstance();
+        // String result = currency.format(1231413.432);
+        // System.out.println(result);
+
+        // NumberFormat percent = NumberFormat.getPercentInstance();
+        // String result = percent.format(0.1);
+        // System.out.println(result);
+
+        String result = NumberFormat.getPercentInstance().format(0.1);
+        System.out.println(result);
     }
 }
